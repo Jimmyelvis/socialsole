1494f02e5ff9dd37b76675d1d9025e9ddfadf395
Jimmyelvis <jinjoe@zoho.com>
2019-09-29 19:04:55 -0500

    Updated changes to articles minor chsnge to app.js

---
+++ models/Article.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+// Create Schema
+const ArticleSchema = new Schema({
+
+  author: {
+    type: String,
+    required: true  
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  headline: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  address:{
+    type: String
+  },
+  fullheaderimage: {
+    type: String
+  },
+  articleheaderimage: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      avatar: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      name: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments:[
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+
+})
+
+module.exports = mongoose.model('article', ArticleSchema);
